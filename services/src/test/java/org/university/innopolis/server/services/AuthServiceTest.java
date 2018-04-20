4644449877cfc37ca5f79750c975a55892263d52
Anton Prokopev <a.prokopev@innopolis.ru>
2018-04-20 15:27:08 +0300

    FIx tests

--- services/src/test/java/org/university/innopolis/server/services/AuthServiceTest.java
+++ services/src/test/java/org/university/innopolis/server/services/AuthServiceTest.java
@@ -3,26 +3,26 @@ package org.university.innopolis.server.services;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
-import org.university.innopolis.server.ServicesConf;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.university.innopolis.server.ServiceConf;
 import org.university.innopolis.server.services.exceptions.BadCredentialsException;
 import org.university.innopolis.server.services.exceptions.DuplicatedUserException;
 import org.university.innopolis.server.services.helpers.ShaHashEncoder;
 import org.university.innopolis.server.views.AccountView;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.Assert.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {ServicesConf.class})
-@DataJpaTest
-
-@EntityScan("org.university.innopolis.server.model")
+@RunWith(SpringRunner.class)
+@DataJpaTest(showSql = false)
+@EnableJpaRepositories(basePackages = "org.university.innopolis.server")
+@EntityScan(basePackages = "org.university.innopolis.server")
+@ContextConfiguration(classes = {ServiceConf.class})
 public class AuthServiceTest {
 
     @Autowired
@@ -31,24 +31,55 @@ public class AuthServiceTest {
     @Autowired
     ShaHashEncoder shaHashEncoder;
 
+    @Autowired
+    private EntityManager entityManager;
+
     private static String password = "password";
     private static String login = "login";
     private static int id = -1;
     private static AccountView view;
 
     @Test
+    public void testSuite() throws Exception {
+        test00();
+        try {
+            test01();
+            fail();
+        } catch (DuplicatedUserException ignored) {
+        }
+
+        test02();
+
+        try {
+            test03();
+            fail();
+        } catch (BadCredentialsException ignored) {
+        }
+
+        try {
+            test04();
+            fail();
+        } catch (BadCredentialsException ignored) {
+        }
+
+        test05();
+        test06();
+        test07();
+        test08();
+        test09();
+        test10();
+    }
+
     public void test00() throws DuplicatedUserException, BadCredentialsException {
         view = authService.registerAccount(login, password);
         assertEquals(login, view.getLogin());
         assertNull(view.getToken());
     }
 
-    @Test(expected = DuplicatedUserException.class)
     public void test01() throws DuplicatedUserException, BadCredentialsException {
         authService.registerAccount(login, "password2");
     }
 
-    @Test
     public void test02() throws BadCredentialsException {
         AccountView res = authService.getAuthentication(login, password);
         assertEquals(login, res.getLogin());
@@ -58,44 +89,39 @@ public class AuthServiceTest {
         view = res;
     }
 
-    @Test(expected = BadCredentialsException.class)
     public void test03() throws BadCredentialsException {
         authService.getAuthentication("notlogin", password);
     }
 
-    @Test(expected = BadCredentialsException.class)
     public void test04() throws BadCredentialsException {
         authService.getAuthentication(login, "notpassword");
     }
 
-    @Test
     public void test05() throws BadCredentialsException {
         AccountView res = authService.getAuthentication(login, password);
         assertEquals(view.getLogin(), res.getLogin());
         assertEquals(view.getToken(), res.getToken());
     }
 
-    @Test
     public void test06() {
         assertNotEquals(-1, id);
         assertEquals(id, authService.getAccountId(view.getToken()));
         authService.revokeToken(view.getToken());
+        entityManager.clear();
         assertEquals(-1, authService.getAccountId(view.getToken()));
     }
 
-    @Test
     public void test07() {
         assertTrue(authService.isAuthorized(id, login));
         assertFalse(authService.isAuthorized(id + 1, login));
     }
 
-    @Test
     public void test08() {
         assertEquals(-1, authService.getAccountId(null));
     }
 
-    @Test
     public void test09() throws BadCredentialsException, InterruptedException {
+        entityManager.clear();
         Thread.sleep(1000);
         AccountView res = authService.getAuthentication(login, password);
         assertNotEquals(view.getToken(), res.getToken());
@@ -104,7 +130,6 @@ public class AuthServiceTest {
         assertEquals(-1, authService.getAccountId(res.getToken()));
     }
 
-    @Test
     public void test10() {
         authService.revokeTokenById(-1);
     }
