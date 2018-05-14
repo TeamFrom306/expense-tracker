5878c23ad11eb97e9a79b4e023f151f49bd8d284
Anton Prokopev <>
2018-05-14 23:14:21 +0300

    Refactor all project
    Reduce all modules to one
    Remove all kafka modules
    Remove all RMI modules

--- services/src/test/java/org/university/innopolis/server/ServiceConf.java
+++
@@ -1,9 +0,0 @@
-package org.university.innopolis.server;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "org.university.innopolis.server")
-public class ServiceConf {
-}
